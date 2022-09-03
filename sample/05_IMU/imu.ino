8f5bc7c38152e35d55c7eecf6d32fa41a4818481
riaosan <fiatpanda750@icloud.com>
2022-09-03 21:02:50 +0900

    add copyright

--- sample/05_IMU/imu.ino
+++ sample/05_IMU/imu.ino
@@ -1,3 +1,27 @@
+/*
+;MIT License
+;
+;Copyright (c) 2021-2022 riraosan.github.io
+;
+;Permission is hereby granted, free of charge, to any person obtaining a copy
+;of this software and associated documentation files (the "Software"), to deal
+;in the Software without restriction, including without limitation the rights
+;to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+;copies of the Software, and to permit persons to whom the Software is
+;furnished to do so, subject to the following conditions:
+;
+;The above copyright notice and this permission notice shall be included in all
+;copies or substantial portions of the Software.
+;
+;THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;SOFTWARE.
+*/
+
 #include <Arduino.h>
 
 #include <M5Atom.h>
@@ -181,12 +205,10 @@ void MPU6886Test() {
     printLine3D(&canvas, &y, TFT_GREEN);
     printLine3D(&canvas, &z, TFT_BLUE);
 
-    /*
     canvas.setTextColor(TFT_WHITE);
     canvas.setTextSize(1);
     canvas.fillRect(0,0,52,18,canvas.color565(20,20,20));
     canvas.drawString("MPU6886",5,5,1);
-    */
 
     last_theta = theta;
     last_phi   = phi;
